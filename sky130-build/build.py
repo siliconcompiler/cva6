2b12c682323f66ba42485024a7cafb3d7039b6ab
Jessica Dowdall <jessica@zeroasic.com>
2022-07-08 22:16:32 +0000

    Clean unnecessary code

--- sky130-build/build.py
+++ sky130-build/build.py
@@ -15,11 +15,8 @@ def setup_options(chip):
         chip.set('option', 'jobname', 'job0')
 
         chip.add('option', 'define', 'WT_DCACHE')
-        
-        cur_dir = os.path.dirname(os.path.realpath(__file__))
-        chip.add('option', 'define', f'MEM_ROOT={cur_dir}')
 
-def configure_chip(remote=False):
+def configure_chip():
         chip = siliconcompiler.Chip('cva6')
 
         setup_options(chip)
@@ -39,25 +36,17 @@ def configure_chip(remote=False):
         chip.set('tool', 'netgen', 'var', 'lvs', '0', 'exclude', ['sky130sram'])
 
         add_sources(chip)
-
-        chip.clock('we_din\[5\]', period=20)
  
         return chip
 
-def build_chip(verify=True, remote=False):
-        chip = configure_chip(remote)
+def build_chip():
+        chip = configure_chip()
         stackup = chip.get('asic', 'stackup')
 
         chip.set('asic', 'diearea', [(0, 0), (5000, 5000)])
         chip.set('asic', 'corearea', [(10, 10), (4990, 4990)]) 
 
-
-        chip.set('model', 'layout', 'lef', stackup, 'cva6.lef')
-
         run_build(chip)
-
-        if verify:
-                run_signoff(chip, 'dfm', 'export')
 
         # set up pointers to final outputs for integration
         gds = chip.find_result('gds', step='export')
@@ -68,19 +57,6 @@ def build_chip(verify=True, remote=False):
         chip.write_manifest('cva6.pkg.json')
 
         return chip
-
-def run_signoff(chip, netlist_step, layout_step):
-    gds_path = chip.find_result('gds', step=layout_step)
-    netlist_path = chip.find_result('vg', step=netlist_step)
-
-    jobname = chip.get('option', 'jobname')
-    chip.set('option', 'jobname', f'{jobname}_signoff')
-    chip.set('option', 'flow', 'signoffflow')
-
-    chip.set('input', 'gds', gds_path)
-    chip.set('input', 'netlist', netlist_path)
-
-    run_build(chip)
 
 def run_build(chip):
         chip.run()
